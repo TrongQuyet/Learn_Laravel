5877346744612ce1245c99d9ed2f61b3acd16a9f
QuyetPirago <quyet.nguyen.trong@pirago.work>
2024-12-05 22:53:22 +0700

    commit1

--- Modules/Web/app/Http/Controllers/AuthController.php
+++ Modules/Web/app/Http/Controllers/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        info('test1');
+        info('test2');
         $validator = Validator::make($request->all(), [
             'name'     => 'required|string|max:255',
             'email'    => 'required|string|email|max:255|unique:users',
