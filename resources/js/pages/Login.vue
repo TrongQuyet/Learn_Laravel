9eade165d77892742434bd9ecccbc7ab3e67e8d7
QuyetPirago <quyet.nguyen.trong@pirago.work>
2024-12-11 16:20:53 +0700

    test2

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -3,7 +3,7 @@
         <div class="login-card">
             <h2>Login1</h2>
             <form @submit.prevent="onLogin">
-                <label for="username">Username:</label>
+                <label for="username">Username1:</label>
                 <input
                     type="text"
                     id="username"
